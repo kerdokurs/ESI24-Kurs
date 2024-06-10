0cc2eb8011fb621881ee6de251c66376a3846c37
Kerdo Kurs <kerdokurs@gmail.com>
2024-06-10 15:04:10 +0300

    [TASK-3] Add kafka event listener to dr service

--- drservice/src/main/java/com/esi/drservice/service/DrService.java
+++ drservice/src/main/java/com/esi/drservice/service/DrService.java
@@ -1,5 +1,9 @@
 package com.esi.drservice.service;
 
+import com.esi.drservice.dto.AppointmentDto;
+import com.esi.drservice.model.Appointment;
+import com.esi.drservice.repository.DrRepository;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -7,5 +11,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class DrService {
+    final DrRepository drRepository;
 
+    @KafkaListener(
+        topics = "appointmentTopic",
+        groupId = "apt-topic-listener-1"
+    )
+    public void onAppointmentRecevied(final AppointmentDto appointmentDto) {
+        var appointment = Appointment.builder()
+            .appointmentid(appointmentDto.getAppointmentid())
+            .patientId(appointmentDto.getPatientId())
+            .patientName(appointmentDto.getPatientName())
+            .drId(appointmentDto.getDrId())
+            .date(appointmentDto.getDate())
+            .build();
+
+        drRepository.save(appointment);
+    }
 }
