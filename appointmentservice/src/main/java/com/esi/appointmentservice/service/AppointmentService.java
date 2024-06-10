466d9f401ec426e73d1933cc011f165828ee8a3d
Kerdo Kurs <kerdokurs@gmail.com>
2024-06-10 14:52:03 +0300

    [TASK-2.2] Add Kafka message sending to apt service

--- appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
+++ appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
@@ -4,12 +4,15 @@ import com.esi.appointmentservice.dto.AppointmentDto;
 import com.esi.appointmentservice.model.Appointment;
 import com.esi.appointmentservice.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
+
+    private final KafkaTemplate<String, AppointmentDto> aptKafkaTemplate;
 
     public void appointmentBooked(AppointmentDto appointmentDto) {
         var appointment = Appointment.builder()
@@ -21,6 +24,7 @@ public class AppointmentService {
             .build();
 
         appointmentRepository.save(appointment);
+        aptKafkaTemplate.send("appointmentTopic", appointmentDto);
     }
 
 }
