497aa8c26f89d89aa1489a597530f09154b2ec9e
Kerdo Kurs <kerdokurs@gmail.com>
2024-06-10 14:44:56 +0300

    [TASK-2.1] Add conversion from apt DTO to apt model and save DB

--- appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
+++ appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
@@ -1,17 +1,26 @@
 package com.esi.appointmentservice.service;
 
-import org.springframework.stereotype.Service;
-
 import com.esi.appointmentservice.dto.AppointmentDto;
-
+import com.esi.appointmentservice.model.Appointment;
+import com.esi.appointmentservice.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class AppointmentService {
+    private final AppointmentRepository appointmentRepository;
 
-  public void appointmentBooked(AppointmentDto appointmentDto) {
+    public void appointmentBooked(AppointmentDto appointmentDto) {
+        var appointment = Appointment.builder()
+            .appointmentid(appointmentDto.getAppointmentid())
+            .patientId(appointmentDto.getPatientId())
+            .patientName(appointmentDto.getPatientName())
+            .drId(appointmentDto.getDrId())
+            .date(appointmentDto.getDate())
+            .build();
 
-  }
+        appointmentRepository.save(appointment);
+    }
 
 }
