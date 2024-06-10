6bbe4554305fe4edc7c6f00648fd02716b0a40a1
Kerdo Kurs <kerdokurs@gmail.com>
2024-06-10 14:40:00 +0300

    [TASK-1] Implement PatientDTO to Patient model conversion and DB save

--- patientservice/src/main/java/com/esi/patientservice/service/PatientService.java
+++ patientservice/src/main/java/com/esi/patientservice/service/PatientService.java
@@ -1,5 +1,7 @@
 package com.esi.patientservice.service;
 
+import com.esi.patientservice.model.Patient;
+import com.esi.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
 import com.esi.patientservice.dto.PatientDto;
@@ -10,8 +12,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PatientService {
 
-    public void addPatientInfo(PatientDto patientDto) {
+    final PatientRepository patientRepository;
 
+    public void addPatientInfo(PatientDto patientDto) {
+        var patient = Patient.builder()
+            .patientId(patientDto.getPatientId())
+            .patientName(patientDto.getPatientName())
+            .patientData(patientDto.getPatientData())
+            .build();
+
+        patientRepository.save(patient);
     }
 
 }
